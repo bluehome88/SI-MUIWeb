21236d6ac35cabdd5fc3ddbab9e2e8cc57ec9a61
bluehome88 <bluehome88@outlook.com>
2021-05-22 11:53:54 +0300

    content page style

--- wp-content/themes/bizipress-child/template-parts/header/content-page-banner.php
+++ wp-content/themes/bizipress-child/template-parts/header/content-page-banner.php
@@ -41,14 +41,23 @@ if (!is_front_page()) { ?>
 	<?php
 		echo bizipress_kses( $overlay );
 	?>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<div class="banner-heading">
-					<h1 class="banner-title"><?php echo esc_html( $heading ); ?></h1>
+	<div class="full-width">
+		<div class="left_div"></div>
+		<div class="container">
+			<div class="row">
+				<div class="banner_card">
+					<label><?php the_title(); ?></label>
+					<h3>All hands on deck</h3>
+					<p>Whether it is your cargo at sea, your personal yacht or water sports equipment, we know how important protecting your investment is.</p>
 				</div>
-			</div><!-- Col end -->
-		</div><!-- Row end -->
-	</div><!-- Container end -->
+				<!--div class="col-xs-12">
+					<div class="banner-heading">
+						<h1 class="banner-title"><?php echo esc_html( $heading ); ?></h1>
+					</div>
+				</div><!-- Col end -->
+			</div><!-- Row end -->
+		</div><!-- Container end -->
+		<div class="right_div"></div>
+	</div>
 </div><!-- Banner area end --> 
 <?php } ?>
