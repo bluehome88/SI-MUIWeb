0cb5111e2a5ed0fc42d5d6b429a67337b43ac133
Blue Webmaster <bluehome88@outlook.com>
2021-06-22 19:53:47 +0300

    display modal for Marine on tab

--- wp-content/themes/bizipress-child/assets/js/custom.js
+++ wp-content/themes/bizipress-child/assets/js/custom.js
@@ -7,4 +7,15 @@ jQuery( document ).ready( function ( $ ) {
     window.location.href = "https://www.massyunitedcargo.com/claims/claim-form";
     return; 
   })
+
+  var country = getCookie('country'); 
+  if( country == "barbados" || country == "jamaica" || country == "caymanislands" || country == "trinidadandtobago"){
+    $(".page.page-id-52 ul.vc_tta-tabs-list .vc_tta-tab a[href='#marine']").on("click", function(e){
+      console.log("button clicked");
+      e.stopPropagation();
+      e.preventDefault();
+
+      $("#exampleModalCenter").modal('show');
+    });
+  }
 });
