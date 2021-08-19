e23963951f2d363fd32bd84dd321c472b07b7f1c
Blue Webmaster <bluehome88@outlook.com>
2021-08-19 05:28:13 +0300

    Cargo claim links for 4 territory

--- wp-content/themes/bizipress-child/assets/js/custom.js
+++ wp-content/themes/bizipress-child/assets/js/custom.js
@@ -1,14 +1,22 @@
 jQuery( document ).ready( function ( $ ) {
   // claim button
+  var country = getCookie('country'); 
   $(".vc_tta-tab a[href='#1622128225297-ce9475cb-8388']").click(function(e){ 
     e.stopPropagation();
     e.preventDefault(); 
-
-    window.location.href = "https://www.massyunitedcargo.com/claims/claim-form";
+  
+  if( country == "barbados" )
+      window.location.href = "/cargo-claim-form-barbados/";
+  if( country == "jamaica" )
+      window.location.href = "/cargo-claim-form-jamaica/";
+  if( country == "caymanislands" )
+      window.location.href = "/cargo-claim-form-cayman-islands/";
+  if( country == "trinidadandtobago" )
+      window.location.href = "/cargo-claim-form-trinidad-and-tobago/";
+      
     return; 
   })
 
-  var country = getCookie('country'); 
   if( country == "barbados" || country == "jamaica" || country == "caymanislands" || country == "trinidadandtobago"){
     $(".page.page-id-52 ul.vc_tta-tabs-list .vc_tta-tab a[href='#marine']").on("click", function(e){
       console.log("button clicked");
